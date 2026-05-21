04f29cdaf7c4fd9d4387d95267fc6c645e57c631
importzey <zeynebhesenova006@gmail.com>
2026-05-21 23:55:15 +0400

    kjsahfksd

--- project.py
+++ project.py
@@ -3,21 +3,28 @@ import sys
 import random 
 
 speed = 13
-
+snake_position=[100, 50]
+snake_body=[[100, 50], [90, 50], [80, 50]]
+running = True
+direction='RIGHT'
+new_direction=direction
 def main():
+    global running
+    global snake_position
+    global snake_body
+    global speed
+    global direction
     pygame.init()
     screen = pygame.display.set_mode((800, 600), pygame.SCALED | pygame.RESIZABLE)
     pygame.display.set_caption("Snake Game")
     clock = pygame.time.Clock()
-    running = True
-    snake_position=[100, 50]
-    snake_body=[[100, 50], [90, 50], [80, 50]]
+
+
     food_position=[random.randrange(1, 80)*10, random.randrange(1, 60)*10]
     food_spawn=True 
     score = 0
   
-    direction='RIGHT'
-    new_direction=direction
+
 
 
     while running:
@@ -27,19 +34,17 @@ def main():
                 running = False
 
         screen.fill("black")
+        
+        score_font = pygame.font.SysFont("Arial", 24)
+        score_surface = score_font.render(f'Score: {score}', True, "white")
+        score_rect = score_surface.get_rect()
+        screen.blit(score_surface, score_rect)
+
         pygame.draw.rect(screen, "red", (*food_position, 10, 10))
         pygame.draw.rect(screen, "green", (*snake_position, 10, 10))
-        
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            new_direction = 'UP'
-        if keys[pygame.K_s]:
-            new_direction = 'DOWN'
-        if keys[pygame.K_a]:
-            new_direction = 'LEFT'
-        if keys[pygame.K_d]:
-            new_direction = 'RIGHT'
-        
+        new_direction = keydirection(keys)
+
         # stop snake to go opposite direction
         if new_direction == 'UP' and direction != 'DOWN':
             direction = new_direction
@@ -63,10 +68,10 @@ def main():
         if snake_position == food_position:
             food_spawn = False  
             score +=10
-            level(score)
+            speed = speed_update(score)
         else:
             snake_body.pop()
-        snake_body.insert(0, list(snake_position))
+        snake_body.insert(0,list(snake_position))
 
         if not food_spawn:
             food_position = [random.randrange(1, 80)*10, random.randrange(1, 60)*10]
@@ -75,14 +80,8 @@ def main():
         for pos in snake_body:
             pygame.draw.rect(screen, "green", pygame.Rect(*pos, 10, 10))
         
-        if snake_position[0] < 0 or snake_position[0] > 790 or snake_position[1] < 0 or snake_position[1] > 590:
-            running = False 
-        
-        for block in snake_body[1:]:
-            if snake_position == block:
-                running = False
- 
-        
+
+        running = is_game_running(running)
         pygame.display.flip()
         clock.tick(speed) 
 
@@ -90,22 +89,42 @@ def main():
     sys.exit()
 
 
-def new_collision():
-    ...
+def keydirection(keys):
+    global direction
+    if keys[pygame.K_w]:
+        return 'UP'
+    if keys[pygame.K_s]:
+        return 'DOWN'
+    if keys[pygame.K_a]:
+        return 'LEFT'
+    if keys[pygame.K_d]:
+        return 'RIGHT'
+    return direction
 
 
-def level(score):
+def speed_update(score):
         global speed
-        if score == 100:
-            speed += 2
+        if score == 80:
+            return speed + 2
         elif score == 150:
-            speed += 3
+            return speed + 4
         elif score == 200:
-            speed += 6
+            return speed + 6
+        elif score == 250:
+            return speed + 8
+        return speed
 
 
-def function_n():
-    ...
+def is_game_running(running):
+    global snake_position
+    global snake_body
+    if snake_position[0] < 0 or snake_position[0] > 790 or snake_position[1] < 0 or snake_position[1] > 590:
+        return False 
+    
+    for block in snake_body[1:]:
+        if snake_position == block:
+            return False
+    return True
 
 
 if __name__ == "__main__":
